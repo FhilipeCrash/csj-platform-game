0de8a893c51e299def1806bbaba433444ae29909
FhilipeCrash <coelhocrash11@gmail.com>
2020-09-22 18:10:29 -0300

    first commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+  public Rigidbody2D rig;
+  public float velocidade;
+  public float tempoNaDirecao;
+
+  float tempo;
+
+  void Start() {
+  }
+  void Update() {
+    tempo += Time.deltaTime;
+
+    if(tempo >= tempoNaDirecao) {
+      velocidade = -velocidade;
+      tempo = 0f;
+    }
+
+    if(velocidade > 0) {
+      transform.eulerAngles = new Vector2(0f, 180f);
+    }
+
+    if(velocidade < 0) {
+      transform.eulerAngles = new Vector2(0f, 0f);
+    }
+
+    rig.velocity = new Vector2(velocidade, rig.velocity.y);
+
+  }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+  public float velocidade;
+  public float forcaPulo;
+  public Rigidbody2D rig;
+  public Animator anim;
+
+  bool estaPulando;
+
+  void Start() {
+    
+  }
+
+  void Update() {
+    Movimentar();
+    Pular();
+  }
+
+  void Movimentar() {
+    float direcao = Input.GetAxis("Horizontal");
+    rig.velocity = new Vector2(direcao * velocidade, rig.velocity.y);
+
+    if(direcao > 0f) {
+      transform.eulerAngles = new Vector2(0f, 0f);
+
+      if(estaPulando == false) {
+        anim.SetInteger("transition", 1);
+      }
+    }
+
+    if(direcao < 0f) {
+      transform.eulerAngles = new Vector2(0f, 180f);
+      if(estaPulando == false) {
+        anim.SetInteger("transition", 1);
+      }
+    }
+
+    if(direcao == 0) {
+      if(estaPulando == false) {
+        anim.SetInteger("transition", 0);
+      }
+    }
+  }
+
+  void Pular() {
+    if(Input.GetButtonDown("Jump") && estaPulando == false) {
+      rig.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
+      estaPulando = true;
+      anim.SetInteger("transition", 2);
+    }
+  }
+  private void OnCollisionEnter2D(Collision2D collision) {
+    if(collision.gameObject.layer == 8) {
+      estaPulando = false;
+    }
+  }
+
+  private void OnTriggerEnter2D(Collider2D collision) {
+    if(collision.CompareTag("enemyBody")) {
+
+    }
+
+    if(collision.CompareTag("enemyHead")) {
+      rig.AddForce(new Vector2(0f, forcaPulo), ForceMode2D.Impulse);
+      Destroy(collision.transform.parent.gameObject);
+    }
+  }
+}
